ee895d366b883027e32899fab80a28955e5e3664
JonyPlo <joonyyplo@gmail.com>
2024-05-26 02:59:09 -0300

    feat: log entity created

---
+++ src/domain/entities/log.entity.ts
@@ -0,0 +1,19 @@
+// Este archivo es llamado 'Entity' o 'Entidad', y una entidad es algo que eventualmente va a terminar llegando a la base de datos, pero no graba directamente en la base de datos, si no que es quien va a gobernar la aplicacion cuando quieramos trabajar con nuestras entidades
+
+export enum LogSeverityLevel {
+  low = 'low',
+  medium = 'medium',
+  high = 'high',
+}
+
+export class logEntity {
+  public level: LogSeverityLevel
+  public message: string
+  public createAt: Date
+
+  constructor(level: LogSeverityLevel, message: string) {
+    this.level = level
+    this.message = message
+    this.createAt = new Date()
+  }
+}
