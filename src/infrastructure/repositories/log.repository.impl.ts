8bfa1d3d72722717a904db554020a349401c5884
JonyPlo <joonyyplo@gmail.com>
2024-06-15 14:08:26 -0300

    refactor: changes comment that talk aboun dependency inyections

--- src/infrastructure/repositories/log.repository.impl.ts
+++ src/infrastructure/repositories/log.repository.impl.ts
@@ -5,8 +5,8 @@ import { LogRepository } from '../../domain/repository/log.repository'
 // La funcion de la implementacion de un repositorio simplemente es recibir algun tipo de datasource y llamar a los metodos del datasource. El objetivos de esta implementacion es que facilmente podamos cambiar el datasource que recibe la implementacion por cualquier otro
 export class LogRepositoryImpl implements LogRepository {
   constructor(
-    //* Crear la propiedad logDataSource de esta forma, es literalmente lo mismo que hacer que el constructor reciba logDataSource como parametro y luego se lo establezca a la propiedad de la clase como 'this.logDataSource = logDataSource'
-    // Esto es una inyeccion de dependencia
+    // Crear la propiedad logDataSource de esta forma, es literalmente lo mismo que hacer que el constructor reciba logDataSource como parametro y luego se lo establezca a la propiedad de la clase como 'this.logDataSource = logDataSource'
+    //* Esto es una inyeccion de dependencia, se conoce como "inyeccion de dependencia" cuando un objeto de tipo clase, en este caso "LogRepositoryImpl", recibe a otro objeto de tipo clase a traves del constructor, en este caso es "logDataSource", entonces la clase logDataSource es una dependencia que se esta inyectando a la clase LogRepositoryImpl/
     private readonly logDataSource: LogDataSource
   ) {}
 
